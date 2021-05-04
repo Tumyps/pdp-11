d32123537411173d58aa991df8c691639d27a670
Tumyps <salomatshoev.ta@phystech.edu>
2021-05-04 16:09:17 +0300

    file from cmd

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "run.c"
+
+int main (int argc, char * argv[]) {
+	char * filename = NULL;
+	if(argc > 1)
+		filename = argv[1];
+	else
+		printf("EMPTY FILE NAME!!!\n");
+
+	load_file(filename);
+	run();
+	return 0;
+}
