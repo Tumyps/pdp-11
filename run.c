48dcb01eb40daa9e738b7431b3ad22bfe36b175e
Tumyps <salomatshoev.ta@phystech.edu>
2021-05-09 01:03:22 +0300

    mode 0,1,2 + check ss,dd

--- run.c
+++ run.c
@@ -2,7 +2,7 @@
 #include <assert.h>
 #include <stdlib.h>
 #include <errno.h>
-#include "mem.c"
+#include "do_func.c"
 
 typedef struct {
 	word mask;
@@ -15,44 +15,39 @@ Command cmd[] = {
 	{0177777, 0000000, "halt", do_halt},
 	{0170000, 0010000, "mov",  do_mov},
 	{0170000, 0060000, "add",  do_add},
-	{0170000, 0000000, "unknown", 	do_unknown}
+	{0000000, 0000000, "unknown", 	do_unknown}
 };
 
-typedef struct 
-{
-	word val;
-	word adr;
-} Arg;
-Arg ss, dd;
 
-Arg get_mr(word w){
+Arg get_mr(word w) {
 	Arg res;
 	int r = w & 7;
 	int mode = (w >> 3) & 7;
-	switch(mode){
-		case 0:		// R3
-			res.adr = r;
-			res.val = reg[r];
-			printf("R%o ", r);
-			break;
-		case 1:		// (R3)
-			res.adr = reg[r];
-			res.val = w_read(res.adr);
-			printf("R%o ", r);
-			break;
-		case 2:		// (R3)+	#3
-			res.adr = reg[r];
-			res.val = w_read(res.adr);
-			reg[r] += 2;
-			if(r == 7)
-				printf("#%o ", res.val);
-			else 
-				printf("(R%o)", r);
-			break;
-		default:
-			fprintf(stderr, "Mode %o NOT IMPLEMENTED YET\n", mode);
-			exit(1);
+	switch (mode) {
+	case 0:		// R3
+		res.adr = r;
+		res.val = reg[r];
+		printf("R%o ", r);
+		break;
+	case 1:		// (R3)
+		res.adr = reg[r];
+		res.val = w_read(res.adr);
+		printf("R%o ", r);
+		break;
+	case 2:		// (R3)+	#3
+		res.adr = reg[r];
+		res.val = w_read(res.adr);
+		reg[r] += 2;
+		if (r == 7)
+			printf("#%o ", res.val);
+		else
+			printf("(R%o)", r);
+		break;
+	default:
+		fprintf(stderr, "Mode %o NOT IMPLEMENTED YET\n", mode);
+		exit(1);
 	}
+	return res;
 }
 
 void run() {
@@ -65,9 +60,12 @@ void run() {
 			Command com = cmd[i];
 			if ((w & com.mask) == com.opcode) {
 				printf("%s\n", com.name);
+				if (com.params & HAS_SS)
+					ss = get_mr(w >> 6);
+				if (com.params & HAS_DD)
+					dd = get_mr(w);
 				com.do_func();
 			}
 		}
 	}
 }
-
