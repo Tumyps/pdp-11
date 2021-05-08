3374bea9bbe447da4b4533e400306226f36fd9fc
Tumyps <salomatshoev.ta@phystech.edu>
2021-05-09 00:59:53 +0300

    halt & mov

---
+++ do_func.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <assert.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "mem.c"
+
+void do_halt() {
+	printf("THE END!!!\n");
+	print_reg();
+	exit(0);
+}
+
+void do_mov() {
+	w_write(dd.adr, ss.val);
+}
+
+void do_add() {
+}
+
+void do_unknown() {
+}
