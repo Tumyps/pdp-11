72dce387e615f6fdb82dd5e89172d74d75bf1e59
Tumyps <salomatshoev.ta@phystech.edu>
2021-05-09 14:55:01 +0300

    fixed includes

--- mem.c
+++ mem.c
@@ -3,47 +3,16 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
-
-typedef unsigned char byte;
-typedef unsigned short word;
-typedef word Adress;
-
-#define MEMSIZE (64 * 1024)
-#define COMMAND_SET_SIZE 4
-#define NO_PARAMS 0
-#define HAS_DD 1
-#define HAS_SS 2
+#include "mem.h"
 
 
 byte mem[MEMSIZE];
 
-word reg[8];
-#define pc reg[7]
-
-typedef struct
-{
-	word val;
-	word adr;
-} Arg;
-Arg ss, dd;
-
-void b_write(Adress adr, byte b);
-byte b_read(Adress adr);
-void w_write(Adress adr, word w);
-word w_read(Adress adr);
-
-void load_file(const char * filename);
-void mem_dump(Adress start, word n);
-void do_add();
-void do_mov();
-void do_halt();
-void do_unknown();
-
-void test_mem()
+/*void test_mem()
 {
 	load_file("data.txt");
 	mem_dump(0x40, 4);
-}
+}*/
 
 word w_read(Adress a) {
 	word w = ((word)mem[a + 1]) << 8;
@@ -96,10 +65,4 @@ void mem_dump(Adress start, word n) {
 	{
 		fprintf(f, "%06o : %06o\n", a, w_read(a));
 	}
-}
-
-void print_reg() {
-	for (int i = 0; i < 6; ++i)
-		printf("R%d=%06o ", i, reg[i]);
-	printf("SP=%06o PC=%06o\n", reg[6], reg[7]);
 }
